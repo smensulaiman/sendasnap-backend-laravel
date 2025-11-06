31d8cb25625eb66551e376c51c464010384da584
MD SULAIMAN <smensulaiman007@gmail.com>
2025-11-06 10:58:31 +0900

    fix: api middleware fix to default

--- routes/api.php
+++ routes/api.php
@@ -27,7 +27,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::prefix('v1')->group(function () {
 
     // Authentication Routes
-    Route::prefix('auth')->middleware('auth:sanctum')->group(function () {
+    Route::prefix('auth')->group(function () {
         Route::post('register', [AuthController::class, 'register']);
         Route::post('login', [AuthController::class, 'login']);
         Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
